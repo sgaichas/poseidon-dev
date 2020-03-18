dc0e30527ac90d192d341efb5173ad1ac290f849
sgaichas <sgaichas@gmail.com>
2020-03-18 15:18:46 -0400

    start on wrapping NOBA cod

---
+++ config/NOBA2Config.R
@@ -0,0 +1,10 @@
+d.name <- here("atlantisoutput","NOBA_march_2020")
+functional.groups.file <- "nordic_groups_v04.csv"
+biomass.pools.file <- "nordic_biol_v23.nc"
+biol.prm.file <- "nordic_biol_incl_harv_v_007_3.prm"
+box.file <- "Nordic02.bgm"
+initial.conditions.file <- "nordic_biol_v23.nc"
+run.prm.file <- "nordic_run_v01.xml"
+scenario.name <- "nordic_runresults_01"
+bioind.file <- "outputnordic_runresults_01BiomIndx.txt"
+catch.file <- "outputnordic_runresults_01Catch.txt"
---
+++ config/codfishery.R
@@ -0,0 +1,41 @@
+# Default fishery configuration here is a census
+# Inherits species from input omlist_ss
+fishspp <- omlist_ss$species_ss 
+
+#Number of years of data to pull
+nyears <- 50
+
+#Atlantis initialization period in years
+burnin <- 30
+
+
+# same time dimensioning parameters as in surveycensus.R
+#Vector of indices of catch in numbers to pull (by timestep to sum)
+fish_sample_full <- c(0:total_sample)  #total_sample defined in sardinesurvey.R
+fish_burnin <- burnin*fstepperyr+1
+fish_nyears <- nyears*fstepperyr
+fish_times <- fish_sample_full[fish_burnin:(fish_burnin+fish_nyears-1)]
+fish_timesteps <- seq(fish_times[5], max(fish_times), by=fstepperyr) #last timestep
+#fish_years <- unique(floor(fish_times/fstepperyr)+1) # my original
+fish_years <- unique(floor(fish_times/fstepperyr)) #from Christine's new sardine_config.R
+
+fishtime <- fish_times
+
+# fishery output: learned the hard way this can be different from ecosystem outputs
+fstepperyr <- if(omlist_ss$runpar$outputstepunit=="days") 365/omlist_ss$runpar$toutfinc
+
+# fishery sampling area
+# should return all model areas, this assumes you see everything that it caught
+fishboxes <- c(0:(omlist_ss$boxpars$nbox - 1))
+
+# effective sample size needed for sample_fish
+# this effective N is high but not equal to total for numerous groups
+fisheffN <- data.frame(species=survspp, effN=rep(1000, length(survspp)))
+
+#this adjusts for subannual fishery output so original effN is for whole year
+fisheffN$effN <- fisheffN$effN/fstepperyr 
+
+# fishery catch cv can be used in sample_survey_biomass
+# perfect observation
+fish_cv <- data.frame(species=survspp, cv=rep(0.01,length(survspp)))
+
