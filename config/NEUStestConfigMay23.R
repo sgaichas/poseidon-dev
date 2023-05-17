87d71e6c1c96b83dc3e05c796afa936cc22c490d
Sarah Gaichas <sarah.gaichas@noaa.gov>
2023-05-17 13:37:01 -0400

    update with all 3 models matching

---
+++ config/NEUStestConfigMay23.R
@@ -0,0 +1,12 @@
+d.name <- here("atlantisoutput","NEUStest_May2023")
+functional.groups.file <- "neus_groups_noturtles.csv"
+biomass.pools.file <- "neus_init.nc"
+biol.prm.file <- "at_biology.prm"
+box.file <- "neus_tmerc_RM2.bgm"
+initial.conditions.file <- "neus_init.nc"
+run.prm.file <- "at_run.xml" #currently missing, only prm version
+scenario.name <- "neus_output"
+bioind.file <- ""
+catch.file <- "neus_outputCatch.txt"
+annage <- TRUE
+fisheries.file <- "neus_fisheries.csv"
